f7c7ff26429f9e31d50c3f870b2a93118b9ba665
ffxxcobraking-dev <ffxxcobraking@gmail.com>
2025-09-22 22:48:39 +0600

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -252,6 +252,7 @@ class InfoCommands(commands.Cog):
                         f"    **├─ BP Badges**: {captain_info.get('badgeCnt', '?')}",
                         f"    **├─ BR Rank**: {'' if captain_info.get('showBrRank') else 'Not found'} {captain_info.get('rankingPoints', 'Not found')}",
                         f"    **└─ CS Rank**: {'' if captain_info.get('showCsRank') else 'Not found'} {captain_info.get('csRankingPoints', 'Not found')} "
+                    "**┌  [click](https://discord.gg/uQG22TaD5v)**"
                     ])
                 embed.add_field(name="", value="\n".join(guild_info), inline=False)
 
